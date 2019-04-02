0d05c1e711418454b0867c1b5e4042389d531484
Yves Dubois-Pèlerin <yves.dubois-pelerin@etat.ge.ch>
2019-04-02 10:46:34 +0200

    OPENSRC-79 Add unit tests : externalize GitLab tree construction from class GitlabTree and merge GitLab and GitLabTree into one class

--- src/main/java/ch/ge/cti_composant/gitSync/missions/PropagateAdminUsersToAllGroups.java
+++ src/main/java/ch/ge/cti_composant/gitSync/missions/PropagateAdminUsersToAllGroups.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import ch.ge.cti_composant.gitSync.util.gitlab.Gitlab;
 import org.gitlab.api.models.GitlabAccessLevel;
 import org.gitlab.api.models.GitlabGroup;
 import org.gitlab.api.models.GitlabGroupMember;
@@ -14,7 +15,6 @@ import org.slf4j.LoggerFactory;
 import ch.ge.cti_composant.gitSync.util.MiscConstants;
 import ch.ge.cti_composant.gitSync.util.MissionUtils;
 import ch.ge.cti_composant.gitSync.util.LDAP.LDAPTree;
-import ch.ge.cti_composant.gitSync.util.gitlab.Gitlab;
 
 /**
  * Ajoute les admins à tous les groupes.
@@ -24,6 +24,7 @@ public class PropagateAdminUsersToAllGroups implements Mission {
 	private static final Logger LOGGER = LoggerFactory.getLogger(PropagateAdminUsersToAllGroups.class);
 
 	@Override
+
 	public void start(LDAPTree ldapTree, Gitlab gitlab) {
 		try {
 			List<GitlabUser> admins = gitlab.getApi().getUsers().stream()
@@ -31,7 +32,7 @@ public class PropagateAdminUsersToAllGroups implements Mission {
 					// Filtre supprimant les admins
 					.filter(admin -> ldapTree.getUsers(MiscConstants.ADMIN_LDAP_GROUP).containsKey(admin.getUsername()))
 					.collect(Collectors.toList());
-			for (GitlabGroup gitlabGroup : gitlab.getTree().getGroups()) {
+			for (GitlabGroup gitlabGroup : gitlab.getGroups()) {
 		    	// Ne pas le faire pour ***REMOVED*** ni ***REMOVED***
 		    	if (!"***REMOVED***".equals(gitlabGroup.getName()) && !"***REMOVED***".equals(gitlabGroup.getName()) ) {
 	   				List<GitlabGroupMember> members = gitlab.getApi().getGroupMembers(gitlabGroup.getId());
