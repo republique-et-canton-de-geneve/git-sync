d98e6ed2456d979561fad645fccd88d11569b521
Yves Dubois-Pèlerin <yves.dubois-pelerin@etat.ge.ch>
2019-04-08 10:26:41 +0200

    OPENSRC-75 Avoid usage of finalizer of Gina LDAP connection + rename class LdapTreeFactory into LdapTreeBuilder

--- src/main/java/ch/ge/cti_composant/gitSync/util/ldap/LdapTreeBuilder.java
+++ src/main/java/ch/ge/cti_composant/gitSync/util/ldap/LdapTreeBuilder.java
@@ -3,8 +3,11 @@ package ch.ge.cti_composant.gitSync.util.ldap;
 /**
  * A object responsible to create a completely initialized {@link LdapTree}.
  */
-public interface LdapTreeFactory {
+public interface LdapTreeBuilder {
 
+    /**
+     * Creates and initializes an {@LdapTree}.
+     */
     LdapTree createTree();
 
 }
