afba066a99209883940e75da235087f0ffed1b8e
Jérôme LAURENT <jerome.laurent@etat.ge.ch>
2022-09-21 13:03:42 +0200

    SCM-915 : add owners group to all groups

---
+++ src/main/java/ch/ge/cti_composant/gitsync/missions/PropagateOwnerUsersToAllGroups.java
@@ -0,0 +1,103 @@
+/*
+ * gitsync
+ *
+ * Copyright (C) 2017-2019 République et canton de Genève
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ch.ge.cti_composant.gitsync.missions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.gitlab.api.models.GitlabAccessLevel;
+import org.gitlab.api.models.GitlabGroupMember;
+import org.gitlab.api.models.GitlabUser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ch.ge.cti_composant.gitsync.util.MissionUtils;
+import ch.ge.cti_composant.gitsync.util.gitlab.Gitlab;
+import ch.ge.cti_composant.gitsync.util.gitlab.GitlabAPIWrapper;
+import ch.ge.cti_composant.gitsync.util.ldap.LdapGroup;
+import ch.ge.cti_composant.gitsync.util.ldap.LdapTree;
+import ch.ge.cti_composant.gitsync.util.ldap.LdapUser;
+
+/**
+ * Adds the owner users to all groups except if already admin.
+ */
+public class PropagateOwnerUsersToAllGroups implements Mission {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PropagateOwnerUsersToAllGroups.class);
+
+    @Override
+    public void start(LdapTree ldapTree, Gitlab gitlab) {
+	LOGGER.info("Propagating owner users to all groups");
+	GitlabAPIWrapper api = gitlab.getApi();
+
+	Map<String, GitlabUser> allUsers = new HashMap<>();
+	api.getUsers().forEach(gitlabUser -> allUsers.put(gitlabUser.getUsername(), gitlabUser));
+
+	String ownerGroup = MissionUtils.getOwnerGroup();
+	LOGGER.info("    Property owner-group is set to [{}]", ownerGroup);
+
+	if (StringUtils.isNotBlank(ownerGroup) && ldapTree.getGroups().contains(new LdapGroup(ownerGroup))) {
+	    // Users in owner group
+	    Map<String, LdapUser> owners = ldapTree.getUsers(ownerGroup);
+
+	    gitlab.getGroups().stream()
+		    // no op for the black-listed groups
+		    .filter(group -> !MissionUtils.getBlackListedGroups().contains(group.getName()))
+		    .filter(group -> MissionUtils.validateGroupnameCompliantStandardGroups(group.getName()))
+		    .forEach(group -> {
+			List<GitlabGroupMember> members = api.getGroupMembers(group);
+
+			owners.forEach((username, ldapUser) -> {
+			    boolean userExists = MissionUtils.validateGitlabUserExistence(ldapUser,
+				    new ArrayList<>(allUsers.values()));
+			    if (userExists) {
+				if (allUsers.get(username).isAdmin()) {
+				    LOGGER.info(
+					    "    User [{}] won't be set as owner to group {} as he is already admin in GitLab",
+					    username, group.getName());
+				}
+				else if (!MissionUtils.isGitlabUserMemberOfGroup(members, username)) {
+				    LOGGER.info("    Setting user [{}] as owner to group {}", username,
+					    group.getName());
+				    api.addGroupMember(group, allUsers.get(username), GitlabAccessLevel.Owner);
+				}
+				else {
+				    LOGGER.info("    Promoting user [{}] as owner to group {}", username,
+					    group.getName());
+				    api.deleteGroupMember(group, allUsers.get(username));
+				    api.addGroupMember(group, allUsers.get(username), GitlabAccessLevel.Owner);
+				}
+			    }
+			    else {
+				LOGGER.info(
+					"    User [{}] won't be set as owner to group {} as it does not exist in GitLab",
+					username, group.getName());
+			    }
+
+			});
+		    });
+	}
+
+	LOGGER.info("Propagating owner users completed");
+    }
+
+}
